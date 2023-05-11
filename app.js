597d250f0ce1805f9c6d9d914df5db2dcb91441c
mohammed afreed <mohammedafreedps111@gmail.com>
2023-05-11 14:56:33 +0530

    version 2

--- app.js
+++ app.js
@@ -1,36 +1,17 @@
-let introblocker = document.querySelector('.intro_blocker')
-let facelogo = document.querySelector('.facelogo-control')
-const myname = document.querySelector('.myname')
-const mainbanner = document.querySelector('.mainbanner')
-let aboutslide = document.querySelector('.aboutme')
-let test = document.querySelector(".about-page")
-let aboutslidefade = document.querySelector(".about-p")
-let body = document.querySelector("body")
 
-window.addEventListener('DOMContentLoaded', () => {
-    setTimeout(()=>{
-        facelogo.classList.add("active")
-    },800);
-    setTimeout(()=>{
-        facelogo.classList.add("disapper")
-    },3300);
-    setTimeout(()=>{
-        introblocker.classList.add("fade")
-       
-    },4000)
-    setTimeout(() => {
-        const observer = new IntersectionObserver(entries => {
-            entries.forEach(entry =>{
-                entry.target.classList.add("slidein")
-            },{
-                threshould: 1
-            })
-            console.log(entries+"myname")
-        })      
-        observer.observe(myname)
-    },4000)
-    setTimeout(()=>{
-        body.classList.add('notactive')
-        mainbanner.classList.add("blur")
-    },3999)
-})
+window.onload = function() {
+    document.getElementById("intro-screen").style.visibility = "visible";
+ };
+
+ document.addEventListener("DOMContentLoaded", function() {
+    // Disable scrolling
+    document.body.style.overflow = "hidden";
+ 
+    // Set a timeout for 3 seconds
+    setTimeout(function() {
+       // Enable scrolling again
+       document.body.style.overflow = "auto";
+    }, 3000);
+ });
+
+ 
---
+++ rought/3drender.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  $('#gallery').addClass('fade-in');
+});
